2383cb832fb7b30a60b77f93aa6692a3eff2bf6c
Alexander Dubois <sander.dubois@outlook.com>
2019-04-08 13:08:13 +0100

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,22 @@
 // Code MovieReviews Here
+
+import React from 'react'
+
+const Review = (props) => {
+    return (
+        <div className="review">
+            <h3>{props.review.display_title}</h3>
+            <p>{props.review.summary_short}</p>
+        </div>
+    )
+}
+
+const MovieReviews = (props) => {
+    return(
+        <div className="review-list">
+            {props.reviews.map( (review, index) => <Review key={index} review={review}/>)}
+        </div>
+    )
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,41 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = 'IhVI7NngfUwItidvhMrxDCxZ11xLD80X';
+const URL = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=${NYT_API_KEY}&query=`
 
-// Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer extends Component {
+
+    state = {
+        reviews: [],
+        serchQuery: ""
+    }
+
+    getReviews() {
+        return fetch(URL + this.state.serchQuery)
+            .then(resp => resp.json())
+            .then(reviews => this.setState({reviews: reviews.results}))
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.setState({serchQuery: event.target.query.value}, () => this.getReviews())
+    }
+
+    render() {
+        return(
+            <div className="searchable-movie-reviews">
+                <h1>Serch through reviews</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Unicorn" name="query"></input>
+                    <input type="submit" value="query"></input>
+                </form>
+                
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
+
+export default SearchableMovieReviewsContainer
